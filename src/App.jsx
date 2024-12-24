884ab2f4a69ac8ac7f41efbb13994af52a2bc45b
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-24 18:04:50 +0600

    add shadcn input, card,

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
-import './App.css'
-import { Button } from './components/ui/button'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <Button>Click me</Button>    </>
+      <Home />
+    </>
   )
 }
 
